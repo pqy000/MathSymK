ed423fc46c7a7c9975fc6df1866c09aac94e1c75
lyc <li-ernest@qq.com>
2024-10-11 14:11:37 +0800

    Drafting NodeMatcher

--- src/main/kotlin/io/github/ezrnest/symbolic/NodeMatcher.kt
+++ src/main/kotlin/io/github/ezrnest/symbolic/NodeMatcher.kt
@@ -7,24 +7,83 @@ interface StructuredRule<T : Node> : SimRule {
     fun simplifyTyped(node: T, context: ExprContext): Node?
 }
 
-interface MatchContext : ExprContext{
+interface MatchContext{
+    val exprContext: ExprContext
 
     val refMap : Map<String, Node>
 }
 
 interface MutableMatchContext : MatchContext {
     override val refMap: MutableMap<String, Node>
+
+    fun copy(): MutableMatchContext
 }
 
-interface NodeMatcher<T> {
+sealed interface NodeMatcher<T : Node> {
 
-    fun matches(node: Node, matchContext: MatchContext): MatchResult<T>?
+    /**
+     * Tries to match the given node with the pattern represented by this matcher, modifying the match context if the match is successful.
+     */
+    fun matches(node: Node, matchContext: MutableMatchContext): T?
+
+    val refNames : Set<String>
 }
 
-interface MatchResult<T> {
-    val matched: T
-    val refMapping: Map<String, T>
+interface TransparentNodeMatcher<T : Node> : NodeMatcher<T> {
+    val child: NodeMatcher<T>
+
 }
+
+interface LeafMatcher<T: Node> : NodeMatcher<T>{
+
+}
+
+interface NodeMatcher1<C : Node> : NodeMatcher<Node1T<C>> {
+    val child: NodeMatcher<C>
+}
+
+interface NodeMatcher2<C1 : Node, C2 : Node> : NodeMatcher<Node2T<C1, C2>> {
+    val child1: NodeMatcher<C1>
+    val child2: NodeMatcher<C2>
+}
+
+interface NodeMatcher3<C1 : Node, C2 : Node, C3 : Node> : NodeMatcher<Node3T<C1, C2, C3>> {
+    val child1: NodeMatcher<C1>
+    val child2: NodeMatcher<C2>
+    val child3: NodeMatcher<C3>
+}
+
+interface MatcherN : NodeMatcher<NodeN> {
+
+}
+
+class AnyMatcher : LeafMatcher<Node>{
+    override fun matches(node: Node, matchContext: MutableMatchContext): Node? {
+        return node
+    }
+
+    override val refNames: Set<String> get() = emptySet()
+}
+
+class RefMatcher(val name: String) : LeafMatcher<Node> {
+    override fun matches(node: Node, matchContext: MutableMatchContext): Node? {
+        val ref = matchContext.refMap[name]
+        if(ref == null){
+            matchContext.refMap[name] = node
+            return node
+        }
+        return if(ref == node) node else null
+    }
+
+    override val refNames: Set<String> get() = setOf(name)
+}
+
+class NamedMatcher
+
+//interface MatchResult<T> {
+//    val matched: T
+//    val refMapping: Map<String, T>
+//}
 
 
 object MatcherBuilderScope {
@@ -48,8 +107,7 @@ fun main() {
         val node = Node.ONE
         val res = m1.matches(node, TODO())
         if (res != null) {
-            val n = res.matched
-            n.second
+            res.second
         }
     }
 }
